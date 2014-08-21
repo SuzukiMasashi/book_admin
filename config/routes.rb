4e87cada481f19bc9c1860d47c72712d08aa6173
SuzukiMasashi <suzuki_masasi@nexway.co.jp>
2014-08-21 14:30:23 +0900

    Add resources :publishers

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   get "/books/:id" => "books#show"
 
+  resources :publishers
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
